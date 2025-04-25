0775ea32a2ac1b346be18f56ae5f38bd1fc94065
Adhishthatri <asunasingh2003@gmail.com>
2025-04-26 00:21:48 +0530

    Fin

---
+++ data/index.ts
@@ -0,0 +1,122 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Have a Research Paper in Blockchain(COMSYS-2024).",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a MedicalStorage app in Blockchain",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Let's Chat - Full Stack Realtime Chat App",
+    des: "Tech stack: MERN + Socket.io + TailwindCSS + Daisy UI",
+    img: "/Let'sChatProject.png",
+    link: "https://lets-chat-app-16mt.onrender.com/",
+  },
+  {
+    id: 2,
+    title: "Weather App",
+    des: "Next JS, React, Tanstack Query, Shadcn UI, Recharts, Tailwind, Typescript ",
+    img: "/klimate.png",
+    link: "https://weather-taupe-tau.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Golds Gym - Fitness App",
+    des: "Modern React 18 Fitness Exercises App With APIs .",
+    img: "/exercise.png",
+    link: "https://fitness-eosin-three.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "CryptoVerse",
+    des: "React Crypto Tracker App- Real Time Data Tracking",
+    img: "/crpto.png",
+    link: "https://crypto-tracker-xi-ivory.vercel.app/",
+  },
+];
+ 
+
+export const socialMedia = [
+  {
+    id: 1,
+    name:"github",
+    link: "https://github.com/Adhishthatri6988",
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    name:"x",
+    link: "https://x.com/AdhishthatriS",
+    img: "/twit.svg",
+  },
+  { id: 3,
+  name:"linkedin",
+  link: "https://linkedin.com/in/adhishthatri-singh-a20583247/",
+  img: "/link.svg",
+},
+];
---
+++ next.config.mjs
@@ -0,0 +1,28 @@
+import { withSentryConfig } from '@sentry/nextjs';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default withSentryConfig(nextConfig, {
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options
+
+  // Suppresses source map uploading logs during build
+  silent: true,
+  org: "javascript-mastery",
+  project: "javascript-nextjs",
+}, {
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+  widenClientFileUpload: true,
+  transpileClientSDK: true,
+  // tunnelRoute: "/monitoring",
+  hideSourceMaps: true,
+  disableLogger: true,
+  automaticVercelMonitors: true,
+});
